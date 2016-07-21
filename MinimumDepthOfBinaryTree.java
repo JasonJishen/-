f6e539b65cd7e5eed8717fdf371986182ff1f04e
JasonJishen <jishen1227@yahoo.com>
2016-07-21 14:28:54 -0400

    Create MinimumDepthOfBinaryTree.java

---
+++ MinimumDepthOfBinaryTree.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int minDepth(TreeNode root) {
+        // write your code here
+        if(root == null) {
+            return 0;
+        }
+        
+        return getMin(root);//用到了辅助函数
+    }
+    
+    public int getMin(TreeNode root){
+        if(root == null) {
+            return Integer.MAX_VALUE;
+        }
+        
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        
+        return Math.min(getMin(root.left),getMin(root.right))+1;//+1加在了min括号里面min(getMin(root.left),getMin(root.right)+1),这个加1很重要不然没法完成递归
+    }
+}
